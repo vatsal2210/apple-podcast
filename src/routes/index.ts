593ee3d7c2c5797927cf75d1765b5a58aaa68432
Vatsal Shah <vatsalshah2210@gmail.com>
2024-09-05 18:25:10 -0400

    Removed react

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,17 @@
 import express from "express";
-import { scrapePodcast, searchPodcasts, searchEpisodesByPerson, getPodcastByGuid, getEpisodeById, downloadEpisode } from "../controller/podcastController";
+import {
+  scrapePodcast,
+  searchPodcasts,
+  searchEpisodesByPerson,
+  getPodcastByGuid,
+  getEpisodeById,
+  downloadEpisode,
+} from "../controller/podcastController";
 
 const router = express.Router();
 
 router.post("/scrape-podcast", scrapePodcast);
-router.get("/search", searchPodcasts);
+router.get("/search/title", searchPodcasts);
 router.get("/search/person", searchEpisodesByPerson);
 router.get("/podcast/:guid", getPodcastByGuid);
 router.get("/episode/:id", getEpisodeById);
